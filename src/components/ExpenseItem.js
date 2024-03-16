d90f74b2bfc350a4dff9a0469e984ed3af606465
sriram057 <sriramsingarapu2@gmail.com>
2024-03-16 22:59:31 +0530

    css and js inside jsx

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,21 @@
+// Write yoour code at relevant places in the code below:
+
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
+  const expenseDate = new Date(2023, 7, 15).toISOString();
+  const expenseTitle = "Insurance";
+  const expenseAmount = 50;
+  const expenseLocation = "Bangalore";
+
   return (
-    <div>
-      <div>15 August 2023</div>
-      <div>
-        <h1>Book</h1>
+    <div className="expense-item">
+      <div>{expenseDate}</div>
+      <div className="expense-item__location">{expenseLocation}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
-      <div>$10</div>
-      <div>Delhi</div>
     </div>
   );
 }
