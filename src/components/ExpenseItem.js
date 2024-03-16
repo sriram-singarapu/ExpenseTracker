bb16011ed67e4bc9950b74c6f8e912ac8939a4bd
sriram057 <sriramsingarapu2@gmail.com>
2024-03-16 22:26:07 +0530

    complex JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,13 @@
 function ExpenseItem() {
   return (
-    <p>
-      <p>Food - $5</p>
-      <p>Petrol - $20</p>
-      <p> Movie - $10</p>
-    </p>
+    <div>
+      <div>15 August 2023</div>
+      <div>
+        <h1>Book</h1>
+      </div>
+      <div>$10</div>
+      <div>Delhi</div>
+    </div>
   );
 }
 
